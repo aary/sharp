68e2d4233453723334415bcd416c1017e8375b82
Aaryaman Sagar <rmn100@gmail.com>
2016-07-24 22:00:56 -0700

    [LockedData] Test case coverage for copy construct
    Added test cases to test the copy construction of the LockedData class

--- LockedData/tests/LockedDataTests.cpp
+++ LockedData/tests/LockedDataTests.cpp
@@ -6,6 +6,33 @@ using namespace sharp;
 namespace sharp {
 class LockedDataTests {
 public:
+    static void test_unique_locked_proxy() {
+        auto fake_mutex = FakeMutex{};
+        auto object = 1;
+        assert(fake_mutex.lock_state == FakeMutex::LockState::UNLOCKED);
+        {
+            auto proxy = LockedData<int, FakeMutex>::UniqueLockedProxy{object,
+                fake_mutex};
+            assert(fake_mutex.lock_state == FakeMutex::LockState::LOCKED);
+            assert(proxy.operator->() == &object);
+            assert(*proxy == 1);
+            assert(&(*proxy) == &object);
+        }
+        assert(fake_mutex.lock_state == FakeMutex::LockState::UNLOCKED);
+
+        // const unique locked proxy shuold read lock the lock
+        assert(fake_mutex.lock_state == FakeMutex::LockState::UNLOCKED);
+        {
+            auto proxy = LockedData<int, FakeMutex>::ConstUniqueLockedProxy{
+                object, fake_mutex};
+            assert(fake_mutex.lock_state == FakeMutex::LockState::SHARED);
+            assert(proxy.operator->() == &object);
+            assert(*proxy == 1);
+            assert(&(*proxy) == &object);
+        }
+        assert(fake_mutex.lock_state == FakeMutex::LockState::UNLOCKED);
+    }
+
     static void test_execute_atomic_non_const() {
         LockedData<double, FakeMutex> locked;
         assert(locked.mtx.lock_state == FakeMutex::LockState::UNLOCKED);
@@ -40,7 +67,7 @@ public:
     }
 
     static void test_lock_const() {
-        LockedData<int, FakeMutex> locked;
+        const LockedData<int, FakeMutex> locked{};
         auto pointer_to_object = reinterpret_cast<intptr_t>(&locked.datum);
         assert(locked.mtx.lock_state == FakeMutex::LockState::UNLOCKED);
         {
@@ -51,11 +78,20 @@ public:
         }
         assert(locked.mtx.lock_state == FakeMutex::LockState::UNLOCKED);
     }
+
+    static void test_copy_constructor() {
+        LockedData<int, FakeMutex> object{};
+        LockedData<int, FakeMutex> copy{object};
+    }
 };
 }
 
 int main() {
+    LockedDataTests::test_unique_locked_proxy();
     LockedDataTests::test_execute_atomic_non_const();
     LockedDataTests::test_execute_atomic_const();
+    LockedDataTests::test_lock();
+    LockedDataTests::test_lock_const();
+    LockedDataTests::test_copy_constructor();
     return 0;
 }
