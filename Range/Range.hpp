230697e1c582459114cb98ebf8fdf965d004ae3b
Aaryaman Sagar <rmn100@gmail.com>
2016-12-28 13:30:03 +0530

    [Range] Added Range header and implementation

--- Range/Range.hpp
+++ Range/Range.hpp
@@ -3,21 +3,25 @@
  * @author Aaryaman Sagar
  *
  * A simple range module like the one in Python, it can be used with the range
- * based for loop syntax and has the same semantics as the one in Python
+ * based for loop syntax and has the same semantics as the one in Python.
+ *
+ * Just for fun ¯\_(ツ)_/¯
  */
 
 #pragma once
 
+#include <iterator>
+#include <type_traits>
+
 namespace sharp {
 
 /**
- * Implementations of the container and iterator that are used to mimic the
- * range fucntionality
+ * Forward declaration for the Range class, this is needed because of C++'s
+ * one pass compile system, without this the function below will not compile
  */
 namespace detail {
-class Range {
-    int begin; int end;
-};
+    template <typename One, typename Two>
+    class Range;
 } // namespace detail
 
 /**
@@ -31,4 +35,92 @@ class Range {
  * inefficiencies from creating objects and iterators will likely be optimized
  * away and the result will be suprisingly similar to a hand written for loop
  */
-detail::Range range(int begin, int end);
+template <typename One, typename Two>
+detail::Range<One, Two> range(One begin, Two end);
+
+/**
+ * Privates!
+ */
+namespace detail {
+
+    /**
+     * Forward declaration for the class Range that is going to be used as the
+     * actual range module, this should never explicitly be instantiated and
+     * hence is in the detail namespace
+     */
+    template <typename One, typename Two>
+    class Range {
+    public:
+
+        /**
+         * Friend the factory function
+         */
+        friend Range<One, Two> sharp::range<One, Two>(One, Two);
+
+        template <typename IncrementableType>
+        class Iterator {
+        public:
+
+            /**
+             * Iterator categories, STL algorithms dont work on some
+             * implementations without these
+             */
+            using difference_type = int;
+            using value_type = IncrementableType;
+            using pointer = std::add_pointer_t<IncrementableType>;
+            using reference = std::add_lvalue_reference_t<IncrementableType>;
+            using iterator_category = std::forward_iterator_tag;
+
+            /**
+             * The constructor for the iterator, adds the incrementable object
+             * as a member variable
+             */
+            Iterator(IncrementableType);
+
+            /**
+             * Increment operator for the iterator
+             */
+            Iterator& operator++();
+
+            /**
+             * Not equals operator
+             */
+            template <typename Incrementable>
+            bool operator!=(Iterator<Incrementable> other) const;
+
+            /**
+             * Return the thing that the iterator points to by value
+             */
+            IncrementableType operator*() const;
+
+        private:
+            IncrementableType incrementable;
+        };
+
+        /**
+         * Begin and end iterators for the range
+         */
+        Iterator<One> begin() const;
+        Iterator<Two> end() const;
+
+    private:
+        /**
+         * Private constructor for this class, can be constructed only with
+         * the range() function defined below
+         */
+        explicit Range(One, Two);
+
+        /**
+         * The members to keep track of what the starting and ending points
+         * were, these cannot be compiled away with constexpr template
+         * varaibles because there can be times when the user wants dynamic
+         * variables as starting and ending points
+         */
+        One first;
+        Two last;
+    };
+
+} // namespace detail
+} // namespace sharp
+
+#include <sharp/Range/Range.ipp>
---
+++ Range/Range.ipp
@@ -0,0 +1,58 @@
+#include <sharp/Range/Range.hpp>
+
+namespace sharp {
+
+template <typename One, typename Two>
+detail::Range<One, Two> range(One begin, Two end) {
+    return detail::Range<One, Two>{begin, end};
+}
+
+namespace detail {
+
+    template <typename One, typename Two>
+    Range<One, Two>::Range(One begin_in, Two end_in)
+        : first{begin_in}, last{end_in} {}
+
+    template <typename One, typename Two>
+    Range<One, Two>::Iterator<One> Range<One, Two>:: begin() const {
+        return Iterator<One>{this->first};
+    }
+
+    template <typename One, typename Two>
+    Range<One, Two>::Iterator<Two> Range<One, Two>::end() const {
+        return Iterator<Two>{this->last};
+    }
+
+    template <typename One, typename Two>
+    template <typename IncrementableType>
+    Range<One, Two>::Iterator<IncrementableType>::Iterator(
+            IncrementableType incrementable_in)
+        : incrementable{incrementable_in} {}
+
+    template <typename One, typename Two>
+    template <typename IncrementableType>
+    IncrementableType Range<One, Two>:: Iterator<IncrementableType>::operator*()
+            const {
+        return this->incrementable;
+    }
+
+
+    template <typename One, typename Two>
+    template <typename IncrementableType>
+    Range<One, Two>::Iterator<IncrementableType>&
+    Range<One, Two>::Iterator<IncrementableType>::operator++() {
+        ++this->incrementable;
+        return *this;
+    }
+
+    template <typename One, typename Two>
+    template <typename IncrementableType>
+    template <typename Incrementable>
+    bool Range<One, Two>::Iterator<IncrementableType>::operator!=(
+            Iterator<Incrementable> other) const {
+        return this->incrementable != other.incrementable;
+    }
+
+} // namespace detail
+
+} // namespace sharp
