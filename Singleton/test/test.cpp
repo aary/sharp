a7d49b80ec5c02ce29968c24c320628dfd5a60e7
Aaryaman Sagar <rmn100@gmail.com>
2017-01-14 08:49:28 -0500

    [Singleton] Added test cases for singleton

---
+++ Singleton/test/test.cpp
@@ -0,0 +1,8 @@
+#include <sharp/Singleton/Singleton.hpp>
+#include <gtest/gtest.h>
+
+TEST(Singleton, simple_test_1) {
+    auto integer_singleton_one = sharp::Singleton<int>::get_strong();
+    auto integer_singleton_two = sharp::Singleton<int>::get_strong();
+    EXPECT_EQ(integer_singleton_one.get(), integer_singleton_two.get());
+}
