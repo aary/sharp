414dfa20528bc6782471907baffcd005b2f4fef0
Aaryaman Sagar <rmn100@gmail.com>
2017-02-10 08:18:10 -0500

    [TransparentList] Moved definition of iterator
    Moved the definition of the iterator class over to the implementation file,
    and kept only the forward declaration in the header file

--- TransparentList/TransparentList.hpp
+++ TransparentList/TransparentList.hpp
@@ -54,8 +54,7 @@ public:
      * required because initializer lists are not deducible via templates.
      */
     template <typename U, typename... Args>
-    Node(sharp::emplace_construct::tag_t,
-         std::initializer_list<U> ilist,
+    Node(sharp::emplace_construct::tag_t, std::initializer_list<U> ilist,
          Args&&... args)
             : datum{std::move(ilist), std::forward<Args>(args)...} {}
 
@@ -87,77 +86,7 @@ public:
     /**
      * An iterator class for generality and convenience
      */
-    class NodeIterator {
-    public:
-
-        /**
-         * These are required to maintain compatibility with STL algorithms
-         *
-         * difference_type denotes a type that can be used to denote the
-         * distance between 2 iterators, see std::distance
-         *
-         * value_type is equivalent to
-         *  std::decay_t<*std::declval<NodeIterator<Type>>()>
-         *
-         * pointer is equivalent to std::add_pointer_t<value_type>
-         *
-         * reference equivalent to std::add_reference_t<value_type>
-         *
-         * iterator_category represents the type of iterator, it can be either
-         * a random access iterator, a forward iterator, a bidirectional
-         * iterator, a reverse iterator, etc
-         */
-        using difference_type = int;
-        using value_type = Node<Type>;
-        using pointer = Node<Type>*;
-        using reference = Node<Type>&;
-        using iterator_category = std::bidirectional_iterator_tag;
-
-        /**
-         * Dereferenece operator
-         */
-        auto& operator*() noexcept;
-
-        /**
-         * Chain operator->, returns a pointer to the held node.  The compiler
-         * chains operator-> calls on everything returned by the call itself,
-         * so calling operator-> on an instance of a NodeIterator is
-         * equivalent to calling operator-> on a Node<Type>* object
-         */
-        Node<Type>* operator->() noexcept;
-
-        /**
-         * Preincrement operator to advance the node pointer to the next spot
-         */
-        NodeIterator& operator++() noexcept;
-
-        /**
-         * Equality operators to determine whether two iterators are pointing to
-         * the same node
-         */
-        bool operator==(const NodeIterator& other) const noexcept;
-        bool operator!=(const NodeIterator& other) const noexcept;
-
-        /**
-         * Become friends with the list class that this iterator is a part of
-         * so that the list class can access the constructor for this class
-         */
-        friend class TransparentList<Type>;
-
-    private:
-
-        /**
-         * The only way to construct an iterator that points to a Node is by
-         * passing a valid node pointer, an assertion fails if the node
-         * pointer is null
-         */
-        NodeIterator(Node<Type>* node_in) noexcept : node_ptr{node_in} {}
-
-        /**
-         * A pointer to the node that the iterator refers to
-         */
-        Node<Type>* node_ptr;
-    };
+    class NodeIterator;
 
     /**
      * A default constructor that initializes the linked list to point to
--- TransparentList/TransparentList.ipp
+++ TransparentList/TransparentList.ipp
@@ -7,6 +7,82 @@
 
 namespace sharp {
 
+/**
+ * An iterator class for generality and convenience
+ */
+template <typename Type>
+class TransparentList<Type>::NodeIterator {
+public:
+
+    /**
+     * These are required to maintain compatibility with STL algorithms
+     *
+     * difference_type denotes a type that can be used to denote the
+     * distance between 2 iterators, see std::distance
+     *
+     * value_type is equivalent to
+     *  std::decay_t<*std::declval<NodeIterator<Type>>()>
+     *
+     * pointer is equivalent to std::add_pointer_t<value_type>
+     *
+     * reference equivalent to std::add_reference_t<value_type>
+     *
+     * iterator_category represents the type of iterator, it can be either
+     * a random access iterator, a forward iterator, a bidirectional
+     * iterator, a reverse iterator, etc
+     */
+    using difference_type = int;
+    using value_type = Node<Type>;
+    using pointer = Node<Type>*;
+    using reference = Node<Type>&;
+    using iterator_category = std::bidirectional_iterator_tag;
+
+    /**
+     * Dereferenece operator
+     */
+    auto& operator*() noexcept;
+
+    /**
+     * Chain operator->, returns a pointer to the held node.  The compiler
+     * chains operator-> calls on everything returned by the call itself,
+     * so calling operator-> on an instance of a NodeIterator is
+     * equivalent to calling operator-> on a Node<Type>* object
+     */
+    Node<Type>* operator->() noexcept;
+
+    /**
+     * Preincrement operator to advance the node pointer to the next spot
+     */
+    NodeIterator& operator++() noexcept;
+
+    /**
+     * Equality operators to determine whether two iterators are pointing to
+     * the same node
+     */
+    bool operator==(const NodeIterator& other) const noexcept;
+    bool operator!=(const NodeIterator& other) const noexcept;
+
+    /**
+     * Become friends with the list class that this iterator is a part of
+     * so that the list class can access the constructor for this class
+     */
+    friend class TransparentList<Type>;
+
+private:
+
+    /**
+     * The only way to construct an iterator that points to a Node is by
+     * passing a valid node pointer, an assertion fails if the node
+     * pointer is null
+     */
+    NodeIterator(Node<Type>* node_in) noexcept : node_ptr{node_in} {}
+
+    /**
+     * A pointer to the node that the iterator refers to
+     */
+    Node<Type>* node_ptr;
+};
+
 template <typename Type>
 static void assert_pointer_invariants(Type* pointer) {
     // assert that the pointer is valid and not null
