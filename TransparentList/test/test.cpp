99bf4f06e72d34e3e03bd6c5256b6ed76fe7fd07
Aaryaman Sagar <rmn100@gmail.com>
2017-02-10 00:42:03 -0500

    [TransparentList] Added tests

--- TransparentList/test/test.cpp
+++ TransparentList/test/test.cpp
@@ -11,12 +11,12 @@ TEST(TransparentList, construct_test) {
     sharp::TransparentList<int>{};
 }
 
-// TEST(TransparentList, simple_push_back_test) {
-    // auto list = sharp::TransparentList<int>{};
-    // auto new_node = new Node<int>{sharp::emplace_construct::tag, 1};
-    // list.push_back(new_node);
-    // EXPECT_EQ(new_node, &(*list.begin()));
-// }
+TEST(TransparentList, simple_push_back_test) {
+    auto list = sharp::TransparentList<int>{};
+    auto new_node = make_unique<Node<int>>(sharp::emplace_construct::tag, 1);
+    list.push_back(new_node.get());
+    EXPECT_EQ(new_node.get(), &(*list.begin()));
+}
 
 TEST(TransparentList, simple_push_front_test) {
     auto list = sharp::TransparentList<int>{};
@@ -26,23 +26,23 @@ TEST(TransparentList, simple_push_front_test) {
     EXPECT_EQ(1, (*list.begin()).datum);
 }
 
-// TEST(TransparentList, double_push_front_test) {
+TEST(TransparentList, double_push_front_test) {
 
-    // auto list = sharp::TransparentList<int>{};
+    auto list = sharp::TransparentList<int>{};
 
-    // auto node_one = make_unique<Node<int>>(sharp::emplace_construct::tag, 1);
-    // list.push_front(node_one.get());
+    auto node_one = make_unique<Node<int>>(sharp::emplace_construct::tag, 1);
+    list.push_front(node_one.get());
 
-    // auto node_two = make_unique<Node<int>>(sharp::emplace_construct::tag, 2);
-    // list.push_front(node_two.get());
+    auto node_two = make_unique<Node<int>>(sharp::emplace_construct::tag, 2);
+    list.push_front(node_two.get());
 
-    // auto node_three = make_unique<Node<int>>(sharp::emplace_construct::tag, 3);
-    // list.push_front(node_three.get());
+    auto node_three = make_unique<Node<int>>(sharp::emplace_construct::tag, 3);
+    list.push_front(node_three.get());
 
-    // EXPECT_EQ(node_three.get(), &(*list.begin()));
-    // EXPECT_EQ(3, (*list.begin()).datum);
-    // EXPECT_EQ(node_two.get(), &(*(++list.begin())));
-    // EXPECT_EQ(2, (*(++list.begin())).datum);
-    // EXPECT_EQ(node_two.get(), &(*(++++list.begin())));
-    // EXPECT_EQ(2, (*(++++list.begin())).datum);
-// }
+    EXPECT_EQ(node_three.get(), &(*list.begin()));
+    EXPECT_EQ(3, (*list.begin()).datum);
+    EXPECT_EQ(node_two.get(), &(*(++list.begin())));
+    EXPECT_EQ(2, (*(++list.begin())).datum);
+    EXPECT_EQ(node_one.get(), &(*(++++list.begin())));
+    EXPECT_EQ(1, (*(++++list.begin())).datum);
+}
