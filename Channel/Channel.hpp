cddc9f288bfffd1adf447266df12d07cfd5b2fbd
Aaryaman Sagar <rmn100@gmail.com>
2017-03-08 00:10:19 -0500

    [channel] Added basic channels implementation

--- Channel/Channel.hpp
+++ Channel/Channel.hpp
@@ -5,6 +5,8 @@
  * This file contains an implementation for channels in C++, these channels
  * work exactly the same as the channels in the Go language, along with the
  * convenience and multiplexing operations supported by channels
+ *
+ * Channels are essentially
  */
 
 #pragma once
@@ -12,9 +14,10 @@
 #include <mutex>
 #include <condition_variable>
 #include <utility>
-#include <queue>
+#include <deque>
 #include <memory>
 #include <exception>
+#include <initializer_list>
 
 #include <sharp/Tags/Tags.hpp>
 
@@ -51,18 +54,14 @@ public:
     Channel(int buffer_length = 0);
 
     /**
-     * Channels are move constructible and default constructible but not copy
-     * constructible
+     * Channels once created cannot be moved or copied around to other
+     * channels.  If the intended use is to have a container of channels, then
+     * the way to use a channel would be to wrap it around a unique_ptr and
+     * then put it in the container
      */
-    Channel(Channel&&);
+    Channel(Channel&&) = delete;
     Channel(const Channel&) = delete;
-
-    /**
-     * Channels are move assignable but not copy assignable, following the
-     * same conventions as std::future, which is essentially a one element
-     * channel without multiplexing capabilities
-     */
-    Channel& operator=(Channel&&);
+    Channel& operator=(Channel&&) = delete;
     Channel& operator=(const Channel&) = delete;
 
     /**
@@ -90,8 +89,21 @@ public:
      */
     void send(const Type& value);
     void send(Type&& value);
+
+    /**
+     * Overloads to send a value and have that constructed in place in the
+     * channel, this avoids unnecesary moves and copies into the channel
+     * storage
+     *
+     * The second function is for initializer list construction in place.
+     * Initializer lists cannot be deduced right from template arguments and
+     * therefore need a second overload
+     */
     template <typename... Args>
     void send(sharp::emplace_construct::tag_t, Args&&... args);
+    template <typename U, typename... Args>
+    void send(sharp::emplace_construct::tag_t, std::initializer_list<U> ilist,
+              Args&&... args);
 
     /**
      * Sends an exception through the channel, this exception if send will be
@@ -133,27 +145,91 @@ public:
      * Make friends with the select function, each select operation waits on a
      * mutex for the channel, and therefore there is some hooking involved
      */
-    template <typename Func, typename... Args>
-    friend void channel_select(std::pair<Channel&, Func> Args&&...);
+    template <typename ChannelType, typename Func, typename... Args>
+    friend void channel_select(std::pair<ChannelType&, Func> Args&&...);
 
 private:
 
     /**
-     * The monitor that is going to be used for this channel
-     *
-     * TODO replace this with a LockedData class object when it is versatile
-     * enough
+     * Function that accepts a range of values and uses that to add an element
+     * into the queue, by perfect forwarding, if a const refernece to a Type
+     * object is passed, then the object in the queue will be copy
+     * constructed, if a rvalue reference is passed in then the object will be
+     * move constructed, if constructor arguments are passed in then the
+     * object will be constructed in place, and otherwise if an initializer
+     * list is passed in along with other arguments, then the same thing will
+     * happen
      */
-    std::condition_variable cv;
+    template <typename... Args>
+    void enqueue_element(Args&&... args);
+    template <typename U, typename... Args>
+    void enqueue_element(std::initializer_list<U> ilist, Args&&... args);
+
+    /**
+     * Wrapper around the send function that does everything send needs to do
+     * but accepts a functor that is used to enqueue an object into the local
+     * queue.  A functor is accepted because of the complications of using an
+     * initializer_list as a template parameter - it cannot be done.  I would
+     * have to write another wrapper that would deal with the initializer_list
+     * send function
+     */
+    template <typename TypeForward, typename Functor>
+    void send_impl(TypeForward&& value, Functor&& enqueue_func);
+
+    /**
+     * The monitor that is going to be used for this channel, there is one
+     * monitor for the senders and one for the readers
+     */
+    std::condition_variable send_cv;
+    std::condition_variable read_cv;
     std::mutex mtx;
+
+    /**
+     * The buffer length, this contains the maximum number of elements that
+     * the buffer in the channel can have, if a sender tries to send more
+     * values through the channel, then they will need to wait until there is
+     * more space in the queue
+     */
+    int buffer_length;
+
+    /**
+     * The number of readers that are waiting for the channel, the sleeping
+     * condition for the senders is that there is enough space in the channel
+     * to send a value or that there is a reader to read the value out of the
+     * channel
+     */
+    int num_readers_waiting;
 
     /**
      * The queue of elements in this channel, this is maintained according to
      * the length of the channel that is passed in as a constructor parameter.
      * The elements in the queue are POD to allow the library to evade default
      * constructions
+     *
+     * TODO change the struct to either be a variant or a union
      */
-    std::queue<std::aligned_storage<sizeof(Type), alignof(Type)>> elements;
+    struct ValueOrException {
+        /**
+         * Storage contains the data for the value that is stored, this is a
+         * trasparent storage medium with no side effects on either
+         * construction or destruction
+         */
+        std::aligned_storage<sizeof(Type), alignof(Type)> storage;
+
+        /**
+         * An exception ptr in the case where the value stored is an exception
+         * that needs to propagate to the user
+         */
+        std::exception_ptr exc_ptr;
+
+        /**
+         * A boolean that denotes whether the value contains an exception or
+         * not, if this is true then the read that reads this value out of the
+         * channel will throw
+         */
+        bool has_exception = false;
+    };
+    std::deque<ValueOrException> elements;
 };
 
 
