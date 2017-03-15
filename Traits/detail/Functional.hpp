856f6d5e970d15d62af847e8aefceb3144af17a0
Aaryaman Sagar <rmn100@gmail.com>
2017-03-15 11:46:01 -0400

    [Traits] Added bind trait

--- Traits/detail/Functional.hpp
+++ Traits/detail/Functional.hpp
@@ -9,3 +9,27 @@
 
 #pragma once
 
+#include <type_traits>
+
+namespace sharp {
+
+/**
+ * @class Bind
+ *
+ * Binds the trait metafunction to the arguments as provided by the type list
+ */
+template <template <typename...> class Trait, typename... ToBind>
+struct Bind {
+    template <typename... Args>
+    struct type {
+        static constexpr const bool value = Trait<ToBind..., Args...>::value;
+    };
+};
+
+/**
+ * Tests for bind
+ */
+static_assert(Bind<std::is_same, int>::type<int>::value,
+        "sharp::Bind tests failed");
+
+} // namespace sharp
