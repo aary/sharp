a0da8d3c7fff133bf3a89836361d0b5167f28823
Aaryaman Sagar <rmn100@gmail.com>
2017-03-15 23:44:59 -0400

    [Traits] Added Concatenate

--- Traits/detail/Algorithm.hpp
+++ Traits/detail/Algorithm.hpp
@@ -28,6 +28,15 @@ namespace sharp {
  */
 struct End {};
 
+/**
+ * @class ValueList
+ *
+ * A constexpr value container, this can be used to store values at compile
+ * time.  At the moment before C++17 this only stores integer values
+ */
+template <int... values>
+struct ValueList {};
+
 namespace detail {
 
     /**
@@ -213,6 +222,22 @@ namespace detail {
                 : MinImpl<second, tail...>::value;
     };
 
+
+    /**
+     * Implemenation for the concatenate trait
+     */
+    template <typename TypesContainerOne, typename TypesContainerTwo>
+    struct ConcatenateImpl;
+    template <typename... TypesOne, typename... TypesTwo>
+    struct ConcatenateImpl<std::tuple<TypesOne...>, std::tuple<TypesTwo...>> {
+        using type = std::tuple<TypesOne..., TypesTwo...>;
+    };
+    template <int... integers_one, int... integers_two>
+    struct ConcatenateImpl<ValueList<integers_one...>,
+                           ValueList<integers_two...>> {
+        using type = ValueList<integers_one..., integers_two...>;
+    };
+
 } // namespace detail
 
 /**
@@ -357,6 +382,18 @@ struct Min {
 };
 
 /**
+ * @class Concatenate
+ *
+ * concatenates two type lists or two value lists, type lists are supported as
+ * std::tuples and value lists are supported as sharp::ValueList
+ */
+template <typename TypesContainerOne, typename TypesContainerTwo>
+struct Concatenate {
+    using type = typename detail::ConcatenateImpl<TypesContainerOne,
+                                                  TypesContainerTwo>::type;
+};
+
+/**
  * Conventional value typedefs, these end in the suffix _v, this is keeping in
  * convention with the C++ standard library features post and including C++17
  */
@@ -391,6 +428,9 @@ using FindFirstOf_t = typename FindFirstOf<
     TypesContainerOne, TypesContainerTwo>::type;
 template <typename... Types>
 using AdjacentFind_t = typename AdjacentFind<Types...>::type;
+template <typename TypesContainerOne, typename TypesContainerTwo>
+using Concatenate_t = typename Concatenate<TypesContainerOne, TypesContainerTwo>
+    ::type;
 
 /*******************************************************************************
  * Tests
@@ -561,5 +601,15 @@ static_assert(std::is_same<AdjacentFind_t<char, int, int>, int>::value,
         "sharp::AdjacentFind tests failed!");
 static_assert(std::is_same<AdjacentFind_t<int*, double&, int*>, End>::value,
         "sharp::AdjacentFind tests failed!");
+
+/**
+ * Tests for Concatenate
+ */
+static_assert(std::is_same<Concatenate_t<std::tuple<int>, std::tuple<double>>,
+                                         std::tuple<int, double>>::value,
+        "sharp::Concatenate tests failed!");
+static_assert(std::is_same<Concatenate_t<ValueList<0>, ValueList<1>>,
+                                         ValueList<0, 1>>::value,
+        "sharp::Concatenate tests failed!");
 
 } // namespace sharp
