9cf14158c90b673ae78d4f025f4cea17c918d1a9
Aaryaman Sagar <rmn100@gmail.com>
2017-04-02 17:25:36 -0400

    [TypeSet] Added test for order of construction

--- TypeSet/test/test.cpp
+++ TypeSet/test/test.cpp
@@ -1,7 +1,18 @@
+#include <vector>
+#include <typeindex>
+
 #include <gtest/gtest.h>
 #include <sharp/TypeSet/TypeSet.hpp>
 
+using namespace std;
 using namespace sharp;
+
+namespace {
+    vector<type_index> order_default_constructs;
+    vector<type_index> order_move_constructs;
+    vector<type_index> order_copy_constructs;
+    vector<type_index> order_destructs;
+}
 
 template <typename Tag>
 class TestConstruct {
@@ -10,24 +21,36 @@ public:
     static int number_move_constructs;
     static int number_copy_constructs;
     static int number_destructs;
+
     static void reset() {
         number_default_constructs = 0;
         number_move_constructs = 0;
         number_copy_constructs = 0;
+        number_destructs = 0;
+        order_default_constructs.clear();
+        order_move_constructs.clear();
+        order_copy_constructs.clear();
+        order_destructs.clear();
     }
 
     TestConstruct() {
         ++number_default_constructs;
+        order_default_constructs.push_back(typeid(Tag));
     }
     TestConstruct(const TestConstruct&) {
         ++number_copy_constructs;
+        order_copy_constructs.push_back(typeid(Tag));
     }
     TestConstruct(TestConstruct&&) {
         ++number_move_constructs;
+        order_move_constructs.push_back(typeid(Tag));
     }
     ~TestConstruct() {
-        ++ number_destructs;
+        ++number_destructs;
+        order_destructs.push_back(typeid(Tag));
     }
+
+    Tag datum;
 };
 
 template <typename Tag>
@@ -49,4 +72,22 @@ TEST(TypeSet, construct_test) {
     EXPECT_EQ(TestConstruct<int>::number_destructs, 1);
     EXPECT_EQ(TestConstruct<double>::number_default_constructs, 1);
     EXPECT_EQ(TestConstruct<double>::number_destructs, 1);
+}
+
+TEST(TypeSet, construct_test_order) {
+    TestConstruct<int>::reset();
+    TestConstruct<double>::reset();
+    {
+        TypeSet<TestConstruct<int>, TestConstruct<double>> ts;
+    }
+    auto order_constructs_correct = vector<type_index>{typeid(int),
+                                                       typeid(double)};
+    auto order_destructs_correct = vector<type_index>{typeid(int),
+                                                      typeid(double)};
+    EXPECT_TRUE(std::equal(order_constructs_correct.begin(),
+                           order_constructs_correct.end(),
+                           order_default_constructs.begin()));
+    EXPECT_TRUE(std::equal(order_destructs_correct.begin(),
+                           order_destructs_correct.end(),
+                           order_destructs.begin()));
 }
