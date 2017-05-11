9242ef65e3388ba50608c169df14e7d8c1196fd9
Aaryaman Sagar <rmn100@gmail.com>
2017-05-11 00:35:53 -0400

    [Future] Added one basic test

--- Future/test/test.cpp
+++ Future/test/test.cpp
@@ -1,3 +1,10 @@
+#include <sharp/Future/Future.hpp>
 #include <gtest/gtest.h>
 
-TEST(Future, FutureBasic) {}
+TEST(Future, FutureBasic) {
+    auto promise = sharp::Promise<int>{};
+    auto future = promise.get_future();
+    promise.set_value(1);
+    auto value = future.get();
+    EXPECT_EQ(value, 1);
+}
