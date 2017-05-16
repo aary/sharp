7a9fa1ac2c716219841540ed0daf1f79f981ca67
Aaryaman Sagar <rmn100@gmail.com>
2017-05-16 12:46:32 -0400

    [Future] Added more tests

--- Future/test/test.cpp
+++ Future/test/test.cpp
@@ -37,6 +37,19 @@ TEST(Future, FutureMove) {
     EXPECT_FALSE(another_future.valid());
 }
 
+TEST(Future, FutureExceptionSend) {
+    auto promise = sharp::Promise<int>{};
+    auto future = promise.get_future();
+    promise.set_exception(std::make_exception_ptr(std::logic_error{""}));
+    try {
+        future.get();
+        EXPECT_TRUE(false);
+    } catch (std::logic_error& err) {
+    } catch (...) {
+        EXPECT_TRUE(false);
+    }
+}
+
 TEST(Future, FutureAlreadyRetrieved) {
     auto promise = sharp::Promise<int>{};
     auto future = promise.get_future();
@@ -129,4 +142,56 @@ TEST(Future, UnwrapConstructBasic) {
         sharp::ThreadTest::mark(0);
         EXPECT_EQ(future_unwrapped.get(), 1);
     }
+}
+
+TEST(Future, UnwrapConstructOtherInvalid) {
+    try {
+        auto future = sharp::Future<sharp::Future<int>>{};
+        auto future_unwrapped = sharp::Future<int>{std::move(future)};
+        EXPECT_TRUE(false);
+    } catch (sharp::FutureError& err) {
+        EXPECT_EQ(err.code().value(), static_cast<int>(
+                    sharp::FutureErrorCode::no_state));
+    }
+}
+
+TEST(Future, UnwrapConstructOtherContainsException) {
+    try {
+        auto promise = sharp::Promise<sharp::Future<int>>{};
+        auto future = promise.get_future();
+        auto future_unwrapped = sharp::Future<int>{std::move(future)};
+        promise.set_exception(std::make_exception_ptr(std::logic_error{""}));
+        future_unwrapped.get();
+        EXPECT_TRUE(false);
+    } catch (std::logic_error& err) {}
+}
+
+TEST(Future, UnwrapConstructOtherContainsInvalid) {
+    try {
+        auto promise = sharp::Promise<sharp::Future<int>>{};
+        auto future = promise.get_future();
+        auto future_unwrapped = sharp::Future<int>{std::move(future)};
+        promise.set_value(sharp::Future<int>{});
+        future_unwrapped.get();
+        EXPECT_TRUE(false);
+    } catch (sharp::FutureError& err) {
+        EXPECT_EQ(err.code().value(), static_cast<int>(
+                    sharp::FutureErrorCode::broken_promise));
+    }
+}
+
+TEST(Future, UnwrapConstructOtherContainsValidWithException) {
+    auto promise = sharp::Promise<sharp::Future<int>>{};
+    auto future = promise.get_future();
+    auto promise_inner = sharp::Promise<int>{};
+    auto future_inner = promise_inner.get_future();
+    auto future_unwrapped = sharp::Future<int>{std::move(future)};
+
+    try {
+        promise_inner.set_exception(
+                std::make_exception_ptr(std::logic_error{""}));
+        promise.set_value(std::move(future_inner));
+        future_unwrapped.get();
+        EXPECT_TRUE(false);
+    } catch (std::logic_error& err) {}
 }
