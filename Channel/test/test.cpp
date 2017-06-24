2277daa937ee609b6bb53d56ec29cde2aa29e874
Aaryaman Sagar <rmn100@gmail.com>
2017-06-24 17:40:46 +0530

    [Channel] Added try_read test

--- Channel/test/test.cpp
+++ Channel/test/test.cpp
@@ -6,3 +6,11 @@ TEST(Channel, BasicTest) {
     ch.send(1);
     EXPECT_EQ(ch.read(), 1);
 }
+
+TEST(Channel, TryReadFail) {
+    sharp::Channel<int> ch_one;
+    EXPECT_FALSE(ch_one.try_read());
+
+    sharp::Channel<int> ch_two{2};
+    EXPECT_FALSE(ch_two.try_read());
+}
