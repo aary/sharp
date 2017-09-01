4529b002f6c60a31da56794c9674abd5bf8a0ba6
Aaryaman Sagar <rmn100@gmail.com>
2017-09-01 08:08:59 -0700

    [Overload] Added test case

--- Overload/test/test.cpp
+++ Overload/test/test.cpp
@@ -2,6 +2,14 @@
 
 #include <gtest/gtest.h>
 
+namespace {
+    int foo(int a) {
+        return a;
+    }
+    double bar(double d) {
+        return d;
+    }
+} // namespace <anonymous>
 
 TEST(Overload, BasicOverloadTest) {
     auto overloaded = sharp::make_overload(
@@ -9,5 +17,28 @@ TEST(Overload, BasicOverloadTest) {
         [](double d) { return d; });
 
     EXPECT_EQ(overloaded(1), 1);
+    EXPECT_TRUE((std::is_same<decltype(overloaded(1)), int>::value));
     EXPECT_EQ(overloaded(2.1), 2.1);
+    EXPECT_TRUE((std::is_same<decltype(overloaded(2.1)), double>::value));
+}
+
+TEST(Overload, BasicOverloadTestFunctionPointers) {
+    auto overloaded = sharp::make_overload(foo, bar);
+
+    EXPECT_EQ(overloaded(1), 1);
+    EXPECT_TRUE((std::is_same<decltype(overloaded(1)), int>::value));
+    EXPECT_EQ(overloaded(1.2), 1.2);
+    EXPECT_TRUE((std::is_same<decltype(overloaded(1.2)), double>::value));
+}
+
+TEST(Overload, FunctionPointerAndFunctor) {
+    auto lambda_one = [](char ch) { return ch; };
+    auto overloaded = sharp::make_overload(lambda_one, foo, bar);
+
+    EXPECT_EQ(overloaded(1), 1);
+    EXPECT_TRUE((std::is_same<decltype(overloaded(1)), int>::value));
+    EXPECT_EQ(overloaded(1.2), 1.2);
+    EXPECT_TRUE((std::is_same<decltype(overloaded(1.2)), double>::value));
+    EXPECT_EQ(overloaded('a'), 'a');
+    EXPECT_TRUE((std::is_same<decltype(overloaded('a')), char>::value));
 }
