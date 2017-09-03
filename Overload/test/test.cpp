ceae42f2b07335941bfb38e1728e509e87bfd7f5
Aaryaman Sagar <rmn100@gmail.com>
2017-09-03 13:30:07 -0700

    [Overload] Fixed errors from merge

--- Overload/test/test.cpp
+++ Overload/test/test.cpp
@@ -35,27 +35,6 @@ TEST(Overload, BasicFunctorOverloadTest) {
     EXPECT_TRUE((std::is_same<decltype(overloaded(2.1)), double>::value));
 }
 
-TEST(Overload, BasicOverloadTestFunctionPointers) {
-    auto overloaded = sharp::make_overload(foo, bar);
-
-    EXPECT_EQ(overloaded(1), 1);
-    EXPECT_TRUE((std::is_same<decltype(overloaded(1)), int>::value));
-    EXPECT_EQ(overloaded(1.2), 1.2);
-    EXPECT_TRUE((std::is_same<decltype(overloaded(1.2)), double>::value));
-}
-
-TEST(Overload, FunctionPointerAndFunctor) {
-    auto lambda_one = [](char ch) { return ch; };
-    auto overloaded = sharp::make_overload(lambda_one, foo, bar);
-
-    EXPECT_EQ(overloaded(1), 1);
-    EXPECT_TRUE((std::is_same<decltype(overloaded(1)), int>::value));
-    EXPECT_EQ(overloaded(1.2), 1.2);
-    EXPECT_TRUE((std::is_same<decltype(overloaded(1.2)), double>::value));
-    EXPECT_EQ(overloaded('a'), 'a');
-    EXPECT_TRUE((std::is_same<decltype(overloaded('a')), char>::value));
-}
-
 TEST(Overload, BasicFunctionOverloadTesT) {
     auto overloaded = sharp::make_overload(foo, bar);
     EXPECT_EQ(overloaded(1), 1);
