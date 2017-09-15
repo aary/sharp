274cfb4a41aa8cdce4245f407f1fed4f4388b116
Aaryaman Sagar <rmn100@gmail.com>
2017-09-14 20:28:37 -0700

    [MoveInto] Fixed some small issues

--- MoveInto/MoveInto.hpp
+++ MoveInto/MoveInto.hpp
@@ -15,21 +15,26 @@ public:
      * The default constructor and any constructor that constructs this from
      * another instance of MoveInto is deleted, an instance of MoveInto can
      * only be constructed by moving an object of type T
+     *
+     * Also delete the copy convert constructor, copies should not be passed
+     * here
      */
     MoveInto() = delete;
-    MoveInto(MoveInto&&) = delete;
     MoveInto(const MoveInto&) = delete;
+    MoveInto(const T&) = delete;
 
     /**
-     * Delete the convert constructor version that would have made a copy
+     * The move constructor needs to be defaulted in C++14 because prvalue
+     * elision is not mandatory and code will not compile if an rvalue is used
+     * to construct a MoveInto instance in a function parameter
      */
-    MoveInto(const T&) = delete;
+    MoveInto(MoveInto&&) = default;
 
     /**
      * The convert move constructor takes an rvalue reference to a T and moves
      * it into the private instance, forcing a move
      */
-    explicit MoveInto(T&& in);
+    MoveInto(T&& in);
 
     /**
      * The destructor is defaulted
@@ -44,7 +49,7 @@ public:
     T&& operator*() &&;
     const T&& operator*() const &&;
     T* operator->();
-    const T* operator->();
+    const T* operator->() const;
 
 private:
     T instance;
