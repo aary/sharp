d34dbabe57391d4d04e10fea84829f75ab24cdb6
Aaryaman Sagar <rmn100@gmail.com>
2017-12-17 22:26:05 -0800

    [Concurrent] Fixed

--- Concurrent/Concurrent.pre.hpp
+++ Concurrent/Concurrent.pre.hpp
@@ -23,10 +23,6 @@
 #include <mutex>
 #include <unordered_map>
 #include <cassert>
-#include <iostream>
-
-using std::cout;
-using std::endl;
 
 namespace sharp {
 namespace concurrent_detail {
@@ -260,9 +256,12 @@ namespace concurrent_detail {
                 for (auto it = this->waiters.begin(); it != this->waiters.end();
                      ++it) {
                     if ((*it)->datum.condition(*proxy)) {
-                        // assert(!(is_reader && (*it)->datum.is_reader));
-                        // cout << "!!!!! About to notify waiter" << endl;
-                        (*it)->datum.notify([]{});
+                        assert(!(is_reader && (*it)->datum.is_reader));
+                        (*it)->datum.notify([&] {
+                            if ((*it)->datum.is_reader) {
+                                (*it)->datum.is_leader = true;
+                            }
+                        });
                         this->waiters.erase(it);
                         break;
                     }
@@ -277,7 +276,7 @@ namespace concurrent_detail {
                   typename LockTag>
         void wait_impl(C condition, LockProxy& proxy,
                        Mtx& mutex,
-                       Lock /* lock */, Unlock /* unlock */,
+                       Lock lock , Unlock unlock,
                        LockQueue lock_queue, UnlockQueue unlock_queue,
                        LockTag) {
             // if the condition is satisfied already at this point then return
@@ -301,21 +300,15 @@ namespace concurrent_detail {
                 waiter.datum.lock();
                 waiter.datum.is_leader = std::is_same<LockTag, WriteLockTag>{};
                 this->notify_impl(proxy, []{}, []{}, LockTag{});
-                // cout << "!!!!! about to push back waiter" << endl;
                 waiters.push_back(&waiter);
-                // cout << "!!!!! pushed back waiter" << endl;
 
                 // unlock the mutex and the queue mutex and prepare to sleep,
                 // at this point the queue contains the waiter which has not
                 // slept yet, but wakeups will not be missed because the
                 // waiter is synchronized that way
-                // cout << "!!!!!! About to unlock normal" << endl;
-                // unlock();
-                // cout << "!!!!!! About to unlock queue" << endl;
+                unlock();
                 unlock_queue();
-                // cout << "!!!!!! About to wait on waiter" << endl;
                 waiter.datum.wait(mutex);
-                // cout << "!!!!!! Woke up" << endl;
 
                 // the order of unlocking the queue mutex and locking the main
                 // mutex is very important, this is needed to prevent against
@@ -325,9 +318,7 @@ namespace concurrent_detail {
                 proxy.is_leader = waiter.datum.is_leader;
                 waiter.datum.unlock();
                 unlock_queue();
-                // cout << "!!!!!! About to acquire lock" << endl;
-                // lock();
-                // cout << "!!!!!! Acquired lock" << endl;
+                lock();
 
                 // there are some things that need doing when threads wake up,
                 // like chaining reader wake calls, transferring stale waiters
@@ -420,10 +411,17 @@ namespace concurrent_detail {
         template <typename C, typename LockProxy>
         void wait(C&& condition, LockProxy& proxy, WriteLockTag) {
             auto& mtx = proxy.instance_ptr->mtx;
-            this->wait_impl(std::forward<C>(condition), proxy, mtx,
-                    [&] { mtx.lock(); }, [&] { mtx.unlock(); },
-                    [&] {}, [&] {},
-                    WriteLockTag{});
+            if (!std::is_same<Mutex, std::mutex>::value) {
+                this->wait_impl(std::forward<C>(condition), proxy, mtx,
+                        [&] { mtx.lock(); }, [&] { mtx.unlock(); },
+                        [&] {}, [&] {},
+                        WriteLockTag{});
+            } else {
+                this->wait_impl(std::forward<C>(condition), proxy, mtx,
+                        [&] {}, [&] {},
+                        [&] {}, [&] {},
+                        WriteLockTag{});
+            }
         }
     };
 
